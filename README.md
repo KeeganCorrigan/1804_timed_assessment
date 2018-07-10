ba407a3f040b2d2b653285818bc2783dfd597b22
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-10 14:22:33 -0600

    add student link functionality to index, all test pass

--- README.md
+++ README.md
@@ -42,11 +42,11 @@ When I visit `/students`
 And I click "Delete" next to a student's name
 I see the students index
 And that student's name is no longer on the page -->
-Navigation
+<!-- Navigation
 As a user
 When I visit any page
 I see links to see a list of all students, or create a new student
-As a user
+As a user -->
 When I visit `/students`
 And I click on a student's name
 I am taken to a show page for that student
--- spec/features/user_sees_student_index_spec.rb
+++ spec/features/user_sees_student_index_spec.rb
@@ -22,4 +22,18 @@ describe "user visits /students" do
     expect(page).to_not have_content(student_1.name)
     expect(Student.all.count).to eq(0)
   end
+
+  it "can link to student by clicking on student name" do
+    student_1 = Student.create!(name: "uiohasd")
+    student_2 = Student.create!(name: "uasdoiasihasd")
+
+    visit students_path
+
+
+
+    click_on student_1.name
+
+    expect(current_path).to eq(student_path(student_1))
+    expect(page).to have_content(student_1.name)
+  end
 end
